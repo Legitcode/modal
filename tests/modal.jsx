bac19b371fdae257bd41797974be908cfd539dec
Zach <zackify@gmail.com>
2015-09-29 22:00:41 -0700

    added test

--- tests/modal.jsx
+++ tests/modal.jsx
@@ -9,9 +9,16 @@ describe('Modal', () => {
   }
 
   it('should return nothing until clicked', () => {
-    Test(<Modal show={false} onClose={close}/>)
+    Test(<Modal show={false} onClose={close}>hey</Modal>)
     .test(({instance}) =>{
       expect(instance.innerHTML).to.be.equal(undefined)
+    })
+  });
+
+  it('should show the modal', () => {
+    Test(<Modal show={true} onClose={close}>hey</Modal>)
+    .test(({instance}) =>{
+      expect(React.findDOMNode(instance).innerHTML).to.match(/div/)
     })
   });
 
